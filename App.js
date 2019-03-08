cbe016f116e2b8d46390fb6dc96d6f19c059051b
prachi <pbhushan@hawk.iit.edu>
2019-03-08 01:09:15 -0600

    initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import Ball from './src/Ball';
+
+
+ class App extends Component() {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Ball />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Ball from './src/Ball';
+
+class Main extends Component {
+ render() {
+     return (
+         <View>
+             <Ball />
+         </View>
+     )
+ }
+};
+export default Main;
